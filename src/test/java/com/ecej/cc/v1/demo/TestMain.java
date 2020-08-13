7e9bb90cc6b9b097f980e3c0f6653002d948efdb
MaLQ <mlq_vip@126.com>
2020-08-13 15:00:55 +0800

    11 合并 22 11
    22
    
    (cherry picked from commit 0e4f38b)

--- src/test/java/com/ecej/cc/v1/demo/TestMain.java
+++ src/test/java/com/ecej/cc/v1/demo/TestMain.java
@@ -26,6 +26,9 @@ public class TestMain {
         System.out.println(22);
         System.out.println(33);
         System.out.println(44);
+        System.out.println(55);
+
+        System.out.println(22);
     }
 
     @Test
